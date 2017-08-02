60639680058017cab9622a0d7e28eeb89885da7e
ginniehench <ginniehench@gmail.com>
2017-08-01 22:15:18 -0400

    I tried to create a header element by targetting the body tag... too many errors. Write straight to the html file

---
+++ main.js
@@ -0,0 +1,22 @@
+let wrapper = document.getElementsByTagName("wrapper");
+let div = document.createElement( "div" );
+//
+// bodyTag.appendChild( header );
+//
+// // let section = document.createElement( "section" );
+//
+// // body.appendChild("div");
+//
+// let h = document.createElement("h1");
+// let hText = document.createTextNode( "Memory: A Game to Stretch Your Life" );
+//
+// h.appendChild( hText );
+//
+// header.appendChild( h );
+//
+//
+//
+//
+// // var h = document.createElement("H1")                // Create a <h1> element
+// // var t = document.createTextNode("Hello World");     // Create a text node
+// // h.appendChild(t);                                   // Append the text to <h1>
