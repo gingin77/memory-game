33203a4d8832de700d6755c206bde2224269bfa1
ginniehench <ginniehench@gmail.com>
2017-08-02 00:23:33 -0400

    lots of things not yet workings

--- main.js
+++ main.js
@@ -1,22 +1,67 @@
-let wrapper = document.getElementsByTagName("wrapper");
-let div = document.createElement( "div" );
+let gamecardUniqueArray = [ "A", "N", "Q", "T", "6", "J", "M", "t", "o"]
+
+let section = document.getElementById( "gameboard_wrapper" );
+// let label = document.createElement("label");
 //
-// bodyTag.appendChild( header );
-//
-// // let section = document.createElement( "section" );
-//
-// // body.appendChild("div");
-//
-// let h = document.createElement("h1");
-// let hText = document.createTextNode( "Memory: A Game to Stretch Your Life" );
-//
-// h.appendChild( hText );
-//
-// header.appendChild( h );
-//
-//
-//
-//
-// // var h = document.createElement("H1")                // Create a <h1> element
-// // var t = document.createTextNode("Hello World");     // Create a text node
-// // h.appendChild(t);                                   // Append the text to <h1>
+// section.appendChild( label );
+
+
+
+for (let i=0; i < gamecardUniqueArray.length; i++){
+  let label = document.createElement("label");
+
+  let input = document.createElement( "input" );
+  let span1 = document.createElement( "span" );
+
+  label.setAttribute( "type", "game title");
+  span.setAttribute( "class", "game-card");
+
+  let span2 = document.createElement( "span" );
+
+  span2.setAttribute( "class", "gamecard-unique");
+
+  label.appendChild( input );
+  label.appendChild( span );
+
+  section.append( label ); //I have no clue why this isn't letting me append... Instead of appendChild, I'm trying parent.append(); https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Below is a starting point for shuffling the cards
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+
+// Read about a class shuffle function, the Fisher-Yates Shuffle, here - https://bost.ocks.org/mike/shuffle/ Copy their 3rd suggestion to get started on something.
